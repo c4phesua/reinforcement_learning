6d1801fb80191807babe4a7fde95d11b1d86fbea
CaroKann <th1475369@gmail.com>
2020-07-10 16:57:37 +0700

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DeepLearning',
+    version='0.1',
+    packages=['rf_model'],
+    url='https://github.com/th1590/reinforcement_learning',
+    license='MIT',
+    author='Carokann',
+    author_email='th1475369@gmail.com',
+    description='reinforcement learning algorithms'
+)
